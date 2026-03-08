9d3c6668fd2e6625ff228b1525f59673ec223cf6
nadendla prasad <prasad.n@syncoms.com>
2026-03-08 12:55:58 +0530

    feat(otp): updated template name

--- src/otp/twofactor.ts
+++ src/otp/twofactor.ts
@@ -3,7 +3,7 @@ import { getLogger } from '../log';
 const logger= getLogger()
 export async function sendOTP(phone: string, code: string) {
     try {
-        await fetch(`https://2factor.in/API/V1/768be78c-cf39-11f0-a6b2-0200cd936042/SMS/${phone}/${code}/LOGIN`)
+        await fetch(`https://2factor.in/API/V1/768be78c-cf39-11f0-a6b2-0200cd936042/SMS/${phone}/${code}/SPARKBUYS`)
     } catch (ex: unknown) {
         logger.error(`failed to send otp ${ex instanceof Error ? ex.message : String(ex)}`)
     }
